f1010352fd251daeda1aee12dd2f5a149fba1c51
kanika <kanika.tiwari@gmail.com>
2019-11-02 16:15:32 +0530

    .

---
+++ Graphs/Basic Graphs.R
@@ -0,0 +1,78 @@
+#Basic Graphs
+# Plots - Scatterplot, matrix plot, marginal plot, dotplot, probability plot, boxplot, interval plot, time series plot, contour plot, individual value plot, area graphs, 
+# Charts - Bar Chart, Pie Chart, 
+# Histogram
+#Stem and Leaf
+
+#Plot
+sale5 <- c(6, 4, 9, 7, 6, 12, 8, 10, 9, 13)
+plot(sale5)
+abline(lsfit(1:10, sale5))
+abline(lsfit(1:10, sale5, intercept = FALSE), col = 4) # less fitting
+
+#BarPlot####
+
+cylinders = table(mtcars$cyl)
+barplot(height=cylinders)
+
+barplot(height=table(mtcars$am))
+barplot(height=table(mtcars$gear), col=1:3, main='No of cars of each gear type', xlab='Gear Type', ylab='Count of Cars')
+
+barplot(height=table(mtcars$carb), col=1:5, horiz = T)
+
+#pie##
+x = c(10,20,40,50)
+pie(x)
+xlabels = c('A','B','C','D')
+pie(x, labels=xlabels)
+
+#Pie with mtcars####
+(cylinders = table(mtcars$cyl))
+pie(x=cylinders)
+pie(x=cylinders, labels=c('4-Cyl','6-Cyl','8-Cyl'))
+pie(x=cylinders, labels=paste(c('4-Cyl','6-Cyl','8-Cyl'),'\n', cylinders, ' cars', sep='-')) 
+
+#Box Plot#####
+?boxplot
+?abline
+boxplot(x=mtcars$mpg)
+abline(h=mean(mtcars$mpg))
+
+#box plot with lines showing quantile values of mpg 
+boxplot(x=mtcars$mpg, col='green')
+abline(h=quantile(mtcars$mpg), col= 1:6)  
+
+#histogram####
+women
+women$height
+?hist
+
+
+#histogram
+hist(women$height, freq=TRUE)  #3 women between 58 and 60
+#no of breaks auto calculated.
+
+hist(women$height, breaks=10)
+hist(women$height, breaks=4, col=1:4)
+#recycle colors if required
+
+#histogram2
+set.seed(123)
+x = rnorm(100,50,100)
+x
+x
+hist(x)
+
+#density
+hist(x, freq=FALSE, col=1:5)  #y axis - density
+#.004 * 100 % = .4% values between 0-50 and so 
+lines(density(x))  #line curve : is it normal : almost normal
+
+#density plot : shape of data
+plot(density(x), col='red')
+
+#mtcars
+hist(mtcars$mpg)
+hist(mtcars$hp)
+
+#end - histogram-----
